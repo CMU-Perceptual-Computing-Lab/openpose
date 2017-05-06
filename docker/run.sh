5a8b65317a8663a2bc82c41073413069987fab9c
ruffsl <roxfoxpox@gmail.com>
2017-05-05 17:01:23 -0700

    Working dockerfile and launch example

--- docker/run.sh
+++ docker/run.sh
@@ -7,7 +7,6 @@ nvidia-docker run \
   --env="DISPLAY" \
   --env="QT_X11_NO_MITSHM=1" \
   --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-  --volume="/${PWD}/../models:/root/openpose/models" \
   --device /dev/video0:/dev/video0 \
   openpose ./build/examples/openpose/rtpose.bin
 
