762b970c6e38b3bd6f64b2d6bcd3c2da73962dee
Gines <gines@cmu.edu>
2017-05-24 18:44:05 -0400

    Fixed OutOfBounds error

--- include/openpose/thread/threadManager.hpp
+++ include/openpose/thread/threadManager.hpp
@@ -464,11 +464,11 @@ namespace op
                     maxQueueId = fastMax(maxQueueId, fastMax(std::get<2>(threadWorkerQueue), std::get<3>(threadWorkerQueue)));
 
                 // Check each queue id has at least a worker that uses it as input and another one as output. Special cases:
-                std::vector<std::pair<bool, bool>> usedQueueIds(maxQueueId, {false, false});
+                std::vector<std::pair<bool, bool>> usedQueueIds(maxQueueId+1, {false, false});
                 for (const auto& threadWorkerQueue : mThreadWorkerQueues)
                 {
-                    usedQueueIds[std::get<2>(threadWorkerQueue)].first = true;
-                    usedQueueIds[std::get<3>(threadWorkerQueue)].second = true;
+                    usedQueueIds.at(std::get<2>(threadWorkerQueue)).first = true;
+                    usedQueueIds.at(std::get<3>(threadWorkerQueue)).second = true;
                 }
                 // Id 0 must only needs a worker using it as input.
                 usedQueueIds.begin()->second = true;
--- src/openpose/utilities/profiler.cpp
+++ src/openpose/utilities/profiler.cpp
@@ -42,7 +42,7 @@ namespace op
             if (sProfilerTuple.count(key) > 0)
                 std::get<2>(sProfilerTuple[key]) = std::chrono::high_resolution_clock::now();
             else
-                sProfilerTuple[key] = {std::make_tuple(0., 0, std::chrono::high_resolution_clock::now())};
+                sProfilerTuple[key] = {std::make_tuple(0., 0ull, std::chrono::high_resolution_clock::now())};
             lock.unlock();
             return key;
         #else
