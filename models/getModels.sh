806135074f1d81a6d1f8518e7089f9fb6dd17d4b
Sri Krishna <skrish13@users.noreply.github.com>
2017-06-30 08:07:35 +0530

    Update getModels.sh

--- models/getModels.sh
+++ models/getModels.sh
@@ -6,19 +6,14 @@ face_folder="face/"
 # Body (COCO)
 coco_folder="$pose_folder"coco/""
 coco_model="$coco_folder"pose_iter_440000.caffemodel""
-if [ ! -f $coco_model ]; then
-    wget -c http://posefs1.perception.cs.cmu.edu/OpenPose/models/$coco_model -P $coco_folder
-fi
+wget -c http://posefs1.perception.cs.cmu.edu/OpenPose/models/$coco_model -P $coco_folder
 
 # Body (MPI)
 mpi_folder="$pose_folder"mpi/""
 mpi_model="$mpi_folder"pose_iter_160000.caffemodel""
-if [ ! -f $mpi_model ]; then
-    wget -c http://posefs1.perception.cs.cmu.edu/OpenPose/models/$mpi_model -P $mpi_folder
-fi
+wget -c http://posefs1.perception.cs.cmu.edu/OpenPose/models/$mpi_model -P $mpi_folder
 
 # Face
 face_model="$face_folder"pose_iter_116000.caffemodel""
-if [ ! -f $face_model ]; then
-    wget -c http://posefs1.perception.cs.cmu.edu/OpenPose/models/$face_model -P $face_folder
-fi
+wget -c http://posefs1.perception.cs.cmu.edu/OpenPose/models/$face_model -P $face_folder
+
