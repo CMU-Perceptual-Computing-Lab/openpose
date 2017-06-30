b31fd8c062f1e23286dc2d016a5e828cd775eea6
Sri Krishna <skrish13@users.noreply.github.com>
2017-06-30 08:01:08 +0530

    Update getModels.sh
    use -c flag in wget s.t resuming can be supported

--- models/getModels.sh
+++ models/getModels.sh
@@ -7,18 +7,18 @@ face_folder="face/"
 coco_folder="$pose_folder"coco/""
 coco_model="$coco_folder"pose_iter_440000.caffemodel""
 if [ ! -f $coco_model ]; then
-    wget http://posefs1.perception.cs.cmu.edu/OpenPose/models/$coco_model -P $coco_folder
+    wget -c http://posefs1.perception.cs.cmu.edu/OpenPose/models/$coco_model -P $coco_folder
 fi
 
 # Body (MPI)
 mpi_folder="$pose_folder"mpi/""
 mpi_model="$mpi_folder"pose_iter_160000.caffemodel""
 if [ ! -f $mpi_model ]; then
-    wget http://posefs1.perception.cs.cmu.edu/OpenPose/models/$mpi_model -P $mpi_folder
+    wget -c http://posefs1.perception.cs.cmu.edu/OpenPose/models/$mpi_model -P $mpi_folder
 fi
 
 # Face
 face_model="$face_folder"pose_iter_116000.caffemodel""
 if [ ! -f $face_model ]; then
-    wget http://posefs1.perception.cs.cmu.edu/OpenPose/models/$face_model -P $face_folder
+    wget -c http://posefs1.perception.cs.cmu.edu/OpenPose/models/$face_model -P $face_folder
 fi
