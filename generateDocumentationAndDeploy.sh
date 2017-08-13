bb32680b465ba8b5fadc3e036de23451ff292c70
bikz05 <bikz.05@gmail.com>
2017-08-13 00:18:05 -0400

    Updated files.

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -70,7 +70,8 @@ echo "" > .nojekyll
 ################################################################################
 ##### Generate the Doxygen code documentation and log the output.          #####
 echo 'Generating Doxygen code documentation...'
-cd doc
+pwd
+cd ${TRAVIS_BUILD_DIR}/doc
 # Redirect both stderr and stdout to the log file AND the console.
 doxygen $DOXYFILE 2>&1 | tee doxygen.log
 
