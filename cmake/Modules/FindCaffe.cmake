c8be20b5bb045b6f2f2e45e8463c4129f5e1614b
bikz05 <bikz.05@gmail.com>
2017-08-14 21:44:06 -0400

    Working on building caffe if needed

--- cmake/Modules/FindCaffe.cmake
+++ cmake/Modules/FindCaffe.cmake
@@ -1,31 +1,19 @@
-# unset(Caffe_FOUND)
+# copied from 
+# https://raw.githubusercontent.com/opencv/opencv_contrib/master/modules/cnn_3dobj/FindCaffe.cmake
 
-###Set the variable Caffe_DIR as the root of your caffe directory
-set(Caffe_DIR ${CMAKE_SOURCE_DIR}/build/3rdparty/caffe)
-
-find_path(Caffe_INCLUDE_DIRS 
-  NAMES 
+find_path(Caffe_INCLUDE_DIR NAMES 
     caffe/caffe.hpp 
     caffe/common.hpp 
     caffe/net.hpp 
     caffe/proto/caffe.pb.h 
-    caffe/util/io.hpp 
-    caffe/vision_layers.hpp
+    caffe/util/io.hpp
+    HINTS
+    /usr/local/include)
+
+find_library(Caffe_LIBS NAMES caffe
   HINTS
-    ${Caffe_DIR}/include)
+  /usr/local/lib)
 
-find_library(Caffe_LIBS 
-  NAMES
-    caffe
-  HINTS
-    ${Caffe_DIR}/lib)
-
-message("include_dirs:${Caffe_INCLUDE_DIRS}")
-message("lib_dirs:${Caffe_LIBS}")
-
-if (Caffe_LIBS AND Caffe_INCLUDE_DIRS)
-  set(Caffe_FOUND true)
-else ()
-  set(Caffe_FOUND false)
-endif ()
-message("\${Caffe_FOUND} = ${Caffe_FOUND}")
+if(CAFFE_LIBS AND Caffe_INCLUDE_DIR)
+    set(Caffe_FOUND 1)
+endif()
