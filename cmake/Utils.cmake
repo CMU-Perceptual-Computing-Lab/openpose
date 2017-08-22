17f066d6b1fc79b183646ca78758b00b151c0539
bikz05 <bikz.05@gmail.com>
2017-08-22 00:05:54 -0400

    Added checksums to downloaded files.

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -1,11 +1,12 @@
 # TODO -- add checksum.
 # TODO -- remove hardcoded ubuntu paths
-function (download_model MODEL_NAME MODEL_DOWNLOAD_FLAG MODEL_RELATIVE_PATH)
+function (download_model MODEL_NAME MODEL_DOWNLOAD_FLAG MODEL_RELATIVE_PATH CHECKSUM)
   if (MODEL_DOWNLOAD_FLAG)
     message(STATUS "Now downloading ${MODEL_NAME} model")
     set(MODEL_FILENAME ${CMAKE_SOURCE_DIR}/models/${MODEL_RELATIVE_PATH})
     if (NOT EXISTS ${MODEL_FILENAME})
-      file(DOWNLOAD ${OPENPOSE_URL}${MODEL_RELATIVE_PATH} ${MODEL_FILENAME}) # SHOW_PROGRESS)
+      file(DOWNLOAD ${OPENPOSE_URL}${MODEL_RELATIVE_PATH} ${MODEL_FILENAME}
+          EXPECTED_MD5 ${CHECKSUM}) # SHOW_PROGRESS)
     else (NOT EXISTS ${MODEL_FILENAME})
       message(STATUS "Model already exists.")
     endif (NOT EXISTS ${MODEL_FILENAME})
