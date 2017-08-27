a9f02b0cc1eb37bce65251dc609ed991e8da2f42
bikz05 <bikz.05@gmail.com>
2017-08-27 01:12:33 -0400

    Flags for installing cuda added.

--- ubuntu/install_cmake.sh
+++ ubuntu/install_cmake.sh
@@ -1,19 +1,51 @@
 #!/bin/bash
 
+echo "Checking Ubuntu Version"
+ubuntu_version="$(lsb_release -r)"
+echo "Ubuntu $ubuntu_version"
+if [[ $ubuntu_version == *"14."* ]]; then
+    ubuntu_le_14=true
+elif [[ $ubuntu_version == *"16."* || $ubuntu_version == *"15."* || $ubuntu_version == *"17."* || $ubuntu_version == *"18."* ]]; then
+    ubuntu_le_14=false
+else
+    echo "Ubuntu release older than version 14. This installation script might fail."
+    ubuntu_le_14=true
+fi
+
 echo "Installing OpenCV and Caffe prerequisites"
+
+
+# Install CUDA -- TODO
+# https://developer.nvidia.com/cuda-downloads
+# Ubuntu 16.04 and CUDA 8
+# TODO -- Add checksums if needed
+
+sudo apt-get update && sudo apt-get install wget -y --no-install-recommends
+if [[ $ubuntu_version == *"14."* ]]; then
+    wget "http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_8.0.61-1_amd64.deb"
+    sudo dpkg -i cuda-repo-ubuntu1404_8.0.61-1_amd64.deb
+elif [[ $ubuntu_version == *"16."* ]]; then
+    wget "http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb"
+    sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+if
+sudo apt-get update
+sudo apt-get install cuda
+
+# Install cuDNN 5.1
+CUDNN_URL="http://developer.download.nvidia.com/compute/redist/cudnn/v5.1/cudnn-8.0-linux-x64-v5.1.tgz"
+wget ${CUDNN_URL}
+sudo tar -xzf cudnn-8.0-linux-x64-v5.1.tgz -C /usr/local
+rm cudnn-8.0-linux-x64-v5.1.tgz && sudo ldconfig
 
 # Basic
 sudo apt-get --assume-yes update
 sudo apt-get --assume-yes install build-essential
-
 # General dependencies
 sudo apt-get --assume-yes install libprotobuf-dev libleveldb-dev libsnappy-dev libhdf5-serial-dev protobuf-compiler
 sudo apt-get --assume-yes install --no-install-recommends libboost-all-dev
 # Remaining dependencies, 14.04
-# if [[ $ubuntu_le_14 == true ]]; then
 sudo apt-get --assume-yes install libgflags-dev libgoogle-glog-dev liblmdb-dev
-# fi
 # Python libs
 sudo -H pip install --upgrade numpy protobuf
-# OpenCV 2.4
+# OpenCV 2.4 -> Added as option
 # sudo apt-get --assume-yes install libopencv-dev
