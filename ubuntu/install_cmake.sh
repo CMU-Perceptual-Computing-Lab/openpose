c9b70630e43e6420b10e6f5721ec8cf1b7a6d0ef
bikz05 <bikz.05@gmail.com>
2017-09-04 19:31:46 -0400

    adding cmake to travis

--- ubuntu/install_cmake.sh
+++ ubuntu/install_cmake.sh
@@ -25,10 +25,11 @@ sudo apt-get --assume-yes update
 sudo apt-get --assume-yes install build-essential
 # General dependencies
 sudo apt-get --assume-yes install libprotobuf-dev libleveldb-dev libsnappy-dev libhdf5-serial-dev protobuf-compiler
+sudo apt-get --assume-yes install --no-install-recommends libboost-all-dev
 if [[ $ubuntu_version == *"14."* ]]; then
+	sudo add-apt-repository ppa:boost-latest/ppa
+	sudo apt-get update
   sudo apt-get --assume-yes install --no-install-recommends libbboost1.55-all-dev
-elif [[ $ubuntu_version == *"16."* ]]; then
-  sudo apt-get --assume-yes install --no-install-recommends libboost-all-dev
 fi
 # Remaining dependencies, 14.04
 sudo apt-get --assume-yes install libgflags-dev libgoogle-glog-dev liblmdb-dev
