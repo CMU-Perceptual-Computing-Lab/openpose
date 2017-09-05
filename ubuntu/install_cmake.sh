2f06ad4e3da0349d299a397c1c1129ad789405b3
bikz05 <bikz.05@gmail.com>
2017-09-04 20:46:47 -0400

    Issue: Caffe cannot find numpy

--- ubuntu/install_cmake.sh
+++ ubuntu/install_cmake.sh
@@ -29,7 +29,6 @@ sudo apt-get --assume-yes install --no-install-recommends libboost-all-dev
 # Remaining dependencies, 14.04
 sudo apt-get --assume-yes install libgflags-dev libgoogle-glog-dev liblmdb-dev
 # Python libs
-sudo pip install protobuf
-sudo apt-get install python-numpy
+sudo -H pip install --upgrade numpy protobuf
 # OpenCV 2.4 -> Added as option
 # sudo apt-get --assume-yes install libopencv-dev
