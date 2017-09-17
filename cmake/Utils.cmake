32a6b7b589df7aa6e4ae47e63362d20f3501f0e6
Gines <gineshidalgo99@gmail.com>
2017-09-16 20:30:47 -0400

    Update Utils.cmake

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -2,10 +2,10 @@
 # TODO -- remove hardcoded ubuntu paths
 function (download_model MODEL_NAME MODEL_DOWNLOAD_FLAG MODEL_RELATIVE_PATH CHECKSUM)
   if (MODEL_DOWNLOAD_FLAG)
-    message(STATUS "Now downloading ${MODEL_NAME} model")
-    message(STATUS "NOTE: Might take time if your internet connection is slow.")
+    message(STATUS "Downloading ${MODEL_NAME} model...")
     set(MODEL_FILENAME ${CMAKE_SOURCE_DIR}/models/${MODEL_RELATIVE_PATH})
     if (NOT EXISTS ${MODEL_FILENAME})
+      message(STATUS "NOTE: This process might take several minutes depending on your internet connection.")
       file(DOWNLOAD ${OPENPOSE_URL}${MODEL_RELATIVE_PATH} ${MODEL_FILENAME}
           EXPECTED_MD5 ${CHECKSUM}) # SHOW_PROGRESS)
     else (NOT EXISTS ${MODEL_FILENAME})
