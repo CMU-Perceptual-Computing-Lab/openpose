8801bcdfe909a20a8c9f5f40ff2db86cee148859
gineshidalgo99 <gineshidalgo99@gmail.com>
2017-11-08 10:27:22 -0500

    Updated Caffe

--- ubuntu/install_cmake.sh
+++ ubuntu/install_cmake.sh
@@ -24,7 +24,7 @@ rm cudnn-8.0-linux-x64-v5.1.tgz && sudo ldconfig
 sudo apt-get --assume-yes update
 sudo apt-get --assume-yes install build-essential
 # General dependencies
-sudo apt-get --assume-yes install libprotobuf-dev libleveldb-dev libsnappy-dev libhdf5-serial-dev protobuf-compiler
+sudo apt-get --assume-yes install libatlas-base-dev libprotobuf-dev libleveldb-dev libsnappy-dev libhdf5-serial-dev protobuf-compiler
 sudo apt-get --assume-yes install --no-install-recommends libboost-all-dev
 # Remaining dependencies, 14.04
 sudo apt-get --assume-yes install libgflags-dev libgoogle-glog-dev liblmdb-dev
