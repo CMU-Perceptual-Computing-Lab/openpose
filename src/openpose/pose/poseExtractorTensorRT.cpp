9e4d903880c9afd427b90b3d9b663ff3000e86ad
Florent Buisson <florent.buisson.1987@gmail.com>
2017-11-14 06:02:22 +0000

    Fixed compilation without TensorRT

--- src/openpose/pose/poseExtractorTensorRT.cpp
+++ src/openpose/pose/poseExtractorTensorRT.cpp
@@ -33,7 +33,7 @@ namespace op
 
     struct PoseExtractorTensorRT::ImplPoseExtractorTensorRT
     {
-        #ifdef USE_TENSORRT // implies USE_CAFFE for now
+        #ifdef USE_TENSORRT // implies USE_TENSORRT for now
             const float mResizeScale;
             std::shared_ptr<Net> spNet;
             std::shared_ptr<ResizeAndMergeCaffe<float>> spResizeAndMergeTensorRT;
@@ -54,21 +54,21 @@ namespace op
                 spNet{std::make_shared<NetTensorRT>(std::array<int,4>{scaleNumber, 3,
                                                     (int)netInputSize.y, (int)netInputSize.x},
                                                     modelFolder + POSE_PROTOTXT[(int)poseModel],
-                                                    modelFolder + POSE_TRAINED_MODEL[(int)poseModel], gpuId)},
+                                                    modelFolder + POSE_TRAINED_MODEL[(int)poseModel], gpuId, enableGoogleLogging)},
                 spResizeAndMergeTensorRT{std::make_shared<ResizeAndMergeCaffe<float>>()},
                 spNmsTensorRT{std::make_shared<NmsCaffe<float>>()},
                 spBodyPartConnectorTensorRT{std::make_shared<BodyPartConnectorCaffe<float>>()}
             {
             }
         #endif
-    }
+    };
  
     PoseExtractorTensorRT::PoseExtractorTensorRT(const Point<int>& netInputSize, const Point<int>& netOutputSize,
                                                  const Point<int>& outputSize, const int scaleNumber,
                                                  const PoseModel poseModel, const std::string& modelFolder, 
                                                  const int gpuId, const std::vector<HeatMapType>& heatMapTypes,
                                                  const ScaleMode heatMapScale, const bool enableGoogleLogging) :
-        PoseExtractor{netOutputSize, outputSize, poseModel, heatMapTypes, heatMapScale},
+        PoseExtractor{netOutputSize, outputSize, poseModel, heatMapTypes, heatMapScale}
         #ifdef USE_TENSORRT
         , upImpl{new ImplPoseExtractorTensorRT{netInputSize, netOutputSize, scaleNumber, poseModel,
                                                gpuId, modelFolder, enableGoogleLogging}}
@@ -94,7 +94,7 @@ namespace op
                 UNUSED(gpuId);
                 UNUSED(heatMapTypes);
                 UNUSED(heatMapScale);
-                error("OpenPose must be compiled with the `USE_CAFFE` macro definition in order to use this"
+                error("OpenPose must be compiled with the `USE_TENSORRT` macro definition in order to use this"
                       " functionality.", __LINE__, __FUNCTION__, __FILE__);
             #endif
         }
@@ -149,58 +149,60 @@ namespace op
     {
         try
         {
-            // Security checks
-            if (inputNetData.empty())
-                error("Empty inputNetData.", __LINE__, __FUNCTION__, __FILE__);
-            timeNow("Start");
-            // 1. TensorRT deep network
-            upImpl->spNet->forwardPass(inputNetData.getConstPtr());
-            timeNow("TensorRT forward");
-            // 2. Resize heat maps + merge different scales
-            upImpl->spResizeAndMergeTensorRT->setScaleRatios(scaleRatios);
-            timeNow("SpResizeAndMergeTensorRT");
-            #ifndef CPU_ONLY
-                upImpl->spResizeAndMergeTensorRT->Forward_gpu({upImpl->spTensorRTNetOutputBlob.get()}, {upImpl->spHeatMapsBlob.get()});       // ~5ms
-                timeNow("RaM forward_gpu");
-                cudaCheck(__LINE__, __FUNCTION__, __FILE__);
-                timeNow("CudaCheck");
-            #else
-                error("ResizeAndMergeTensorRT CPU version not implemented yet.", __LINE__, __FUNCTION__, __FILE__);
-            #endif
-            timeNow("Resize heat Maps");
-            // 3. Get peaks by Non-Maximum Suppression
-            upImpl->spNmsTensorRT->setThreshold((float)get(PoseProperty::NMSThreshold));
-            #ifndef CPU_ONLY
-                upImpl->spNmsTensorRT->Forward_gpu({upImpl->spHeatMapsBlob.get()}, {upImpl->spPeaksBlob.get()});                           // ~2ms
-                cudaCheck(__LINE__, __FUNCTION__, __FILE__);
-            #else
-                error("NmsTensorRT CPU version not implemented yet.", __LINE__, __FUNCTION__, __FILE__);
-            #endif
-            timeNow("Peaks by nms");
-            // Get scale net to output
-            const auto scaleProducerToNetInput = resizeGetScaleFactor(inputDataSize, mNetOutputSize);
-            const Point<int> netSize{intRound(scaleProducerToNetInput*inputDataSize.x), intRound(scaleProducerToNetInput*inputDataSize.y)};
-            mScaleNetToOutput = {(float)resizeGetScaleFactor(netSize, mOutputSize)};
-            timeNow("Scale net to output");
-            // 4. Connecting body parts
-            upImpl->spBodyPartConnectorTensorRT->setScaleNetToOutput(mScaleNetToOutput);
-            upImpl->spBodyPartConnectorTensorRT->setInterMinAboveThreshold((int)get(PoseProperty::ConnectInterMinAboveThreshold));
-            upImpl->spBodyPartConnectorTensorRT->setInterThreshold((float)get(PoseProperty::ConnectInterThreshold));
-            upImpl->spBodyPartConnectorTensorRT->setMinSubsetCnt((int)get(PoseProperty::ConnectMinSubsetCnt));
-            upImpl->spBodyPartConnectorTensorRT->setMinSubsetScore((float)get(PoseProperty::ConnectMinSubsetScore));
-            // GPU version not implemented yet
-            upImpl->spBodyPartConnectorTensorRT->Forward_cpu({upImpl->spHeatMapsBlob.get(), upImpl->spPeaksBlob.get()}, mPoseKeypoints);
-            // upImpl->spBodyPartConnectorTensorRT->Forward_gpu({upImpl->spHeatMapsBlob.get(), upImpl->spPeaksBlob.get()}, {upImpl->spPoseBlob.get()}, mPoseKeypoints);
-            timeNow("Connect Body Parts");
-             
-            const auto totalTimeSec = timeDiffToString(timings.back().second, timings.front().second);
-            const auto message = "Pose estimation successfully finished. Total time: " + totalTimeSec + " seconds.";
-            op::log(message, op::Priority::High);
+            #ifdef USE_TENSORRT
+                // Security checks
+                if (inputNetData.empty())
+                    error("Empty inputNetData.", __LINE__, __FUNCTION__, __FILE__);
+                timeNow("Start");
+                // 1. TensorRT deep network
+                upImpl->spNet->forwardPass(inputNetData.getConstPtr());
+                timeNow("TensorRT forward");
+                // 2. Resize heat maps + merge different scales
+                upImpl->spResizeAndMergeTensorRT->setScaleRatios(scaleRatios);
+                timeNow("SpResizeAndMergeTensorRT");
+                #ifndef CPU_ONLY
+                    upImpl->spResizeAndMergeTensorRT->Forward_gpu({upImpl->spTensorRTNetOutputBlob.get()}, {upImpl->spHeatMapsBlob.get()});       // ~5ms
+                    timeNow("RaM forward_gpu");
+                    cudaCheck(__LINE__, __FUNCTION__, __FILE__);
+                    timeNow("CudaCheck");
+                #else
+                    error("ResizeAndMergeTensorRT CPU version not implemented yet.", __LINE__, __FUNCTION__, __FILE__);
+                #endif
+                timeNow("Resize heat Maps");
+                // 3. Get peaks by Non-Maximum Suppression
+                upImpl->spNmsTensorRT->setThreshold((float)get(PoseProperty::NMSThreshold));
+                #ifndef CPU_ONLY
+                    upImpl->spNmsTensorRT->Forward_gpu({upImpl->spHeatMapsBlob.get()}, {upImpl->spPeaksBlob.get()});                           // ~2ms
+                    cudaCheck(__LINE__, __FUNCTION__, __FILE__);
+                #else
+                    error("NmsTensorRT CPU version not implemented yet.", __LINE__, __FUNCTION__, __FILE__);
+                #endif
+                timeNow("Peaks by nms");
+                // Get scale net to output
+                const auto scaleProducerToNetInput = resizeGetScaleFactor(inputDataSize, mNetOutputSize);
+                const Point<int> netSize{intRound(scaleProducerToNetInput*inputDataSize.x), intRound(scaleProducerToNetInput*inputDataSize.y)};
+                mScaleNetToOutput = {(float)resizeGetScaleFactor(netSize, mOutputSize)};
+                timeNow("Scale net to output");
+                // 4. Connecting body parts
+                upImpl->spBodyPartConnectorTensorRT->setScaleNetToOutput(mScaleNetToOutput);
+                upImpl->spBodyPartConnectorTensorRT->setInterMinAboveThreshold((int)get(PoseProperty::ConnectInterMinAboveThreshold));
+                upImpl->spBodyPartConnectorTensorRT->setInterThreshold((float)get(PoseProperty::ConnectInterThreshold));
+                upImpl->spBodyPartConnectorTensorRT->setMinSubsetCnt((int)get(PoseProperty::ConnectMinSubsetCnt));
+                upImpl->spBodyPartConnectorTensorRT->setMinSubsetScore((float)get(PoseProperty::ConnectMinSubsetScore));
+                // GPU version not implemented yet
+                upImpl->spBodyPartConnectorTensorRT->Forward_cpu({upImpl->spHeatMapsBlob.get(), upImpl->spPeaksBlob.get()}, mPoseKeypoints);
+                // upImpl->spBodyPartConnectorTensorRT->Forward_gpu({upImpl->spHeatMapsBlob.get(), upImpl->spPeaksBlob.get()}, {upImpl->spPoseBlob.get()}, mPoseKeypoints);
+                timeNow("Connect Body Parts");
+                 
+                const auto totalTimeSec = timeDiffToString(timings.back().second, timings.front().second);
+                const auto message = "Pose estimation successfully finished. Total time: " + totalTimeSec + " seconds.";
+                op::log(message, op::Priority::High);
 
-            for(OpTimings::iterator timing = timings.begin()+1; timing != timings.end(); ++timing) {
-              const auto log_time = (*timing).first + " - " + timeDiffToString((*timing).second, (*(timing-1)).second);
-              op::log(log_time, op::Priority::High);
-            }
+                for(OpTimings::iterator timing = timings.begin()+1; timing != timings.end(); ++timing) {
+                  const auto log_time = (*timing).first + " - " + timeDiffToString((*timing).second, (*(timing-1)).second);
+                  op::log(log_time, op::Priority::High);
+                }
+            #endif
         }
         catch (const std::exception& e)
         {
@@ -211,9 +213,13 @@ namespace op
     const float* PoseExtractorTensorRT::getHeatMapCpuConstPtr() const
     {
         try
-        {
-            checkThread();
-            return upImpl->spHeatMapsBlob->cpu_data();
+        {    
+            #ifdef USE_TENSORRT
+                checkThread();
+                return upImpl->spHeatMapsBlob->cpu_data();
+            #else
+                return nullptr;
+            #endif
         }
         catch (const std::exception& e)
         {
@@ -226,7 +232,7 @@ namespace op
     {
         try
         {
-            #ifdef USE_CAFFE
+            #ifdef USE_TENSORRT
                 checkThread();
                 return upImpl->spHeatMapsBlob->gpu_data();
             #else
@@ -244,7 +250,7 @@ namespace op
     {
         try
         {
-            #ifdef USE_CAFFE
+            #ifdef USE_TENSORRT
                 checkThread();
                 return upImpl->spHeatMapsBlob->shape();
             #else
@@ -262,7 +268,7 @@ namespace op
     {
         try
         {
-            #ifdef USE_CAFFE
+            #ifdef USE_TENSORRT
                 error("GPU pointer for people pose data not implemented yet.", __LINE__, __FUNCTION__, __FILE__);
                 checkThread();
                 return upImpl->spPoseBlob->gpu_data();
