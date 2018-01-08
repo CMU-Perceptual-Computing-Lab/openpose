80d8e9f3810a5ffeb0fab0bc888d5a9aaf1ed6d7
Raaj <ryaadhav@andrew.cmu.edu>
2018-01-08 11:25:57 -0500

    added a caffeutil function class to display blob size

---
+++ src/openpose/utilities/caffeutil.cpp
@@ -0,0 +1,14 @@
+#ifdef USE_CUDA
+    #include <cuda.h>
+    #include <cuda_runtime.h>
+    #include <openpose/utilities/fastMath.hpp>
+#endif
+#ifdef USE_CAFFE
+    #include <caffe/blob.hpp>
+#endif
+#include <openpose/utilities/cuda.hpp>
+
+namespace op
+{
+
+}
