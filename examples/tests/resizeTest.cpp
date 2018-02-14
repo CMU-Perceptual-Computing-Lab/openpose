a3c7b0a6770a5c10a87411f1774a169a3ec3c669
Raaj <ryaadhav@andrew.cmu.edu>
2018-02-14 12:35:49 -0500

    coding style change

--- examples/tests/resizeTest.cpp
+++ examples/tests/resizeTest.cpp
@@ -17,43 +17,45 @@ namespace gflags = google;
 
 DEFINE_string(image_path,               "examples/media/COCO_val2014_000000000192.jpg",     "Process the desired image.");
 
-cv::Mat gpuResize(cv::Mat& img, cv::Size newSize){
-#ifdef USE_CUDA
-    // Upload to Source to GPU
-    float* cpuPtr = &img.at<float>(0);
-    float* gpuPtr;
-    cudaMallocHost((void **)&gpuPtr, img.size().width * img.size().height * sizeof(float));
-    cudaMemcpy(gpuPtr, cpuPtr, img.size().width * img.size().height * sizeof(float),
-               cudaMemcpyHostToDevice);
+cv::Mat gpuResize(cv::Mat& img, cv::Size newSize)
+{
+    #ifdef USE_CUDA
+        // Upload to Source to GPU
+        float* cpuPtr = &img.at<float>(0);
+        float* gpuPtr;
+        cudaMallocHost((void **)&gpuPtr, img.size().width * img.size().height * sizeof(float));
+        cudaMemcpy(gpuPtr, cpuPtr, img.size().width * img.size().height * sizeof(float),
+                   cudaMemcpyHostToDevice);
 
-    // Upload to Dest to GPU
-    cv::Mat newImg = cv::Mat(newSize,CV_32FC1,cv::Scalar(0));
-    float* newCpuPtr = &newImg.at<float>(0);
-    float* newGpuPtr;
-    cudaMallocHost((void **)&newGpuPtr, newSize.width * newSize.height * sizeof(float));
-    cudaMemcpy(newGpuPtr, newCpuPtr, newSize.width * newSize.height * sizeof(float),
-               cudaMemcpyHostToDevice);
+        // Upload to Dest to GPU
+        cv::Mat newImg = cv::Mat(newSize,CV_32FC1,cv::Scalar(0));
+        float* newCpuPtr = &newImg.at<float>(0);
+        float* newGpuPtr;
+        cudaMallocHost((void **)&newGpuPtr, newSize.width * newSize.height * sizeof(float));
+        cudaMemcpy(newGpuPtr, newCpuPtr, newSize.width * newSize.height * sizeof(float),
+                   cudaMemcpyHostToDevice);
 
-    std::vector<const float*> sourcePtrs;
-    sourcePtrs.emplace_back(gpuPtr);
-    std::array<int, 4> targetSize = {1,1,newImg.size().height,newImg.size().width};
-    std::array<int, 4> sourceSize = {1,1,img.size().height,img.size().width};
-    std::vector<std::array<int, 4>> sourceSizes;
-    sourceSizes.emplace_back(sourceSize);
-    op::resizeAndMergeGpu(newGpuPtr, sourcePtrs, targetSize, sourceSizes);
-    cudaMemcpy(newCpuPtr, newGpuPtr, newImg.size().width * newImg.size().height * sizeof(float),
-               cudaMemcpyDeviceToHost);
+        std::vector<const float*> sourcePtrs;
+        sourcePtrs.emplace_back(gpuPtr);
+        std::array<int, 4> targetSize = {1,1,newImg.size().height,newImg.size().width};
+        std::array<int, 4> sourceSize = {1,1,img.size().height,img.size().width};
+        std::vector<std::array<int, 4>> sourceSizes;
+        sourceSizes.emplace_back(sourceSize);
+        op::resizeAndMergeGpu(newGpuPtr, sourcePtrs, targetSize, sourceSizes);
+        cudaMemcpy(newCpuPtr, newGpuPtr, newImg.size().width * newImg.size().height * sizeof(float),
+                   cudaMemcpyDeviceToHost);
 
-    cudaFree(gpuPtr);
-    cudaFree(newGpuPtr);
-    return newImg;
-#else
-    op::error("OpenPose must be compiled with the `USE_CAFFE` & `USE_CUDA` macro definitions in order to run"
-          " this functionality.", __LINE__, __FUNCTION__, __FILE__);
-#endif
+        cudaFree(gpuPtr);
+        cudaFree(newGpuPtr);
+        return newImg;
+    #else
+        op::error("OpenPose must be compiled with the `USE_CAFFE` & `USE_CUDA` macro definitions in order to run"
+              " this functionality.", __LINE__, __FUNCTION__, __FILE__);
+    #endif
 }
 
-cv::Mat cpuResize(cv::Mat& img, cv::Size newSize){
+cv::Mat cpuResize(cv::Mat& img, cv::Size newSize)
+{
     // Upload to Source to GPU
     float* cpuPtr = &img.at<float>(0);
 
