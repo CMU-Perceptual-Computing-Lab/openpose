a51d3e23233daeda48fff86883ccfa4100c6ac83
Raaj <yaadhavraaj@gmail.com>
2018-05-09 13:23:05 -0400

    wip personTracker

--- src/openpose/experimental/tracking/personTracker.cpp
+++ src/openpose/experimental/tracking/personTracker.cpp
@@ -1,5 +1,6 @@
 #include <thread>
 #include <openpose/experimental/tracking/personTracker.hpp>
+#include <iostream>
 
 namespace op
 {
@@ -27,10 +28,53 @@ namespace op
     {
     }
 
-    void personEntriesFromOP(std::unordered_map<int, PersonTrackerEntry>& personEntries, Array<float>& poseKeypoints)
+    void personEntriesFromOP(std::unordered_map<int, PersonTrackerEntry>& personEntries, const Array<float>& poseKeypoints, const Array<long long>& poseIds)
     {
         personEntries.clear();
-        wip
+        for(int i=0; i<poseKeypoints.getSize()[0]; i++)
+        {
+            auto id = poseIds[i];
+            personEntries[id] = PersonTrackerEntry();
+            personEntries[id].keypoints.resize(poseKeypoints.getSize()[1]);
+            personEntries[id].status.resize(poseKeypoints.getSize()[1]);
+            for(int j=0; j<poseKeypoints.getSize()[1]; j++)
+            {
+                personEntries[id].keypoints[j].x = poseKeypoints[
+                        i*poseKeypoints.getSize()[1]*poseKeypoints.getSize()[2] +
+                        j*poseKeypoints.getSize()[2] + 0];
+                personEntries[id].keypoints[j].y = poseKeypoints[
+                        i*poseKeypoints.getSize()[1]*poseKeypoints.getSize()[2] +
+                        j*poseKeypoints.getSize()[2] + 1];
+                float prob = poseKeypoints[
+                        i*poseKeypoints.getSize()[1]*poseKeypoints.getSize()[2] +
+                        j*poseKeypoints.getSize()[2] + 2];
+                if(prob < 0.05) personEntries[id].status[j] = 0;
+                else personEntries[id].status[j] = 1;
+            }
+        }
+    }
+
+    void OPFromPersonEntries(Array<float>& poseKeypoints, std::unordered_map<int, PersonTrackerEntry>& personEntries)
+    {
+        if (!personEntries.size())
+            return;
+        int dims[] = { (int)personEntries.size(), (int)personEntries.begin()->second.keypoints.size(), 3 };
+        cv::Mat opArrayMat(3,dims,CV_32FC1);
+        int i=0;
+        for (auto& kv : personEntries)
+        {
+            const PersonTrackerEntry& pe = kv.second;
+            for (int j=0; j<dims[1]; j++)
+            {
+                opArrayMat.at<float>(i*dims[1]*dims[2] + j*dims[2] + 0) = pe.keypoints[j].x;
+                opArrayMat.at<float>(i*dims[1]*dims[2] + j*dims[2] + 1) = pe.keypoints[j].y;
+                opArrayMat.at<float>(i*dims[1]*dims[2] + j*dims[2] + 2) = !(int)pe.status[j];
+                if (pe.keypoints[j].x == 0 && pe.keypoints[j].y == 0)
+                    opArrayMat.at<float>(i*dims[1]*dims[2] + j*dims[2] + 2) = 0;
+            }
+            i++;
+        }
+        poseKeypoints.setFrom(opArrayMat);
     }
 
     void PersonTracker::track(Array<float>& poseKeypoints, Array<long long>& poseIds,
@@ -38,18 +82,25 @@ namespace op
     {
         try
         {
+            std::cout << poseKeypoints.getSize()[0] << std::endl;
+            std::cout << mMergeResults << std::endl;
+            std::cout << "---" << std::endl;
 
             // Sanity Checks
             if(!poseKeypoints.empty() && !poseIds.empty())
                 if(poseKeypoints.getSize()[0] != poseIds.getSize()[0])
                      error("poseKeypoints and poseIds should have the same number of people", __LINE__, __FUNCTION__, __FILE__);
 
-            // First frame
-            if(mImagePrevious.empty()){
-                // Create mPersonEntries
+//            // First frame
+//            if(mImagePrevious.empty()){
+//                // Create mPersonEntries
+//                personEntriesFromOP(mPersonEntries, poseKeypoints, poseIds);
 
-            }
 
+//            }
+
+
+            // REMEMBER TO FLIP THE LK VALUES!!
 
             /*
              * 1. Get poseKeypoints for all people - Checks
