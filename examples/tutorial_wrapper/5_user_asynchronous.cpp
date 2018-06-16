8401f85edba1db375980c7dabe8ac6aa659027c2
gineshidalgo99 <gineshidalgo99@gmail.com>
2018-06-16 13:00:37 -0400

    Fixed bug

--- examples/tutorial_wrapper/5_user_asynchronous.cpp
+++ examples/tutorial_wrapper/5_user_asynchronous.cpp
@@ -427,7 +427,7 @@ int openPoseTutorialWrapper3()
             op::stringToDataFormat(FLAGS_write_keypoint_format), FLAGS_write_json, FLAGS_write_coco_json,
             FLAGS_write_coco_foot_json, FLAGS_write_images, FLAGS_write_images_format, FLAGS_write_video,
             FLAGS_camera_fps, FLAGS_write_heatmaps, FLAGS_write_heatmaps_format, FLAGS_write_video_adam,
-            FLAGS_write_bvh, udp_host, udp_port};
+            FLAGS_write_bvh, FLAGS_udp_host, FLAGS_udp_port};
         // Configure wrapper
         opWrapper.configure(wrapperStructPose, wrapperStructFace, wrapperStructHand, wrapperStructExtra,
                             op::WrapperStructInput{}, wrapperStructOutput);
