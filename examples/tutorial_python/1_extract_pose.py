51fac83af431801b997f78e768b6c4ac5225b66f
Raaj <yaadhavraaj@gmail.com>
2018-06-26 05:00:07 +0800

    Update 1_extract_pose.py

--- examples/tutorial_python/1_extract_pose.py
+++ examples/tutorial_python/1_extract_pose.py
@@ -1,12 +1,21 @@
+# From Python
+# It requires OpenCV installed for Python
 import sys
 import cv2
 import os
 from sys import platform
+
+# Remember to add your installation path here
+# Option a
 dir_path = os.path.dirname(os.path.realpath(__file__))
 if platform == "win32": sys.path.append(dir_path + '/../../python/openpose/');
 else: sys.path.append('../../python');
-from openpose import *
+# Option b
+# If you run `make install` (default path is `/usr/local/python` for Ubuntu), you can also access the OpenPose/python module from there. This will install OpenPose and the python library at your desired installation path. Ensure that this is in your python path in order to use it.
+# sys.path.append('/usr/local/python')
 
+# Parameters for OpenPose. Take a look at C++ OpenPose example for meaning of components. Ensure all below are filled
+from openpose import *
 params = dict()
 params["logging_level"] = 3
 params["output_resolution"] = "-1x-1"
@@ -16,14 +25,21 @@ params["alpha_pose"] = 0.6
 params["scale_gap"] = 0.3
 params["scale_number"] = 1
 params["render_threshold"] = 0.05
+# If GPU version is built, and multiple GPUs are available, set the ID here
 params["num_gpu_start"] = 0
 params["disable_blending"] = False
 params["default_model_folder"] = dir_path + "/../../../models/"
+# Construct OpenPose object allocates GPU memory
 openpose = OpenPose(params)
-img = cv2.imread(dir_path + "/../../../examples/media/COCO_val2014_000000000192.jpg")
-arr, output_image = openpose.forward(img, True)
-print(arr)
 
+# Read new image
+img = cv2.imread(dir_path + "/../../../examples/media/COCO_val2014_000000000192.jpg")
+# Output keypoints and the image with the human skeleton blended on it
+keypoints, output_image = openpose.forward(img, True)
+# Print the human pose keypoints, i.e., a [#people x #keypoints x 3]-dimensional numpy object with the keypoints of all the people on that image
+print(keypoints)
+
+# Display the image
 while 1:
     cv2.imshow("output", output_image)
     cv2.waitKey(15)
