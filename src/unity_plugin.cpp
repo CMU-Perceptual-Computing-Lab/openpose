205b5b978a76426713a908e14291a6f44dc6d271
Tianyi Zhao <tz1@andrew.cmu.edu>
2018-10-28 20:26:57 -0400

    solve crash problem + add face rect

--- src/unity_plugin.cpp
+++ src/unity_plugin.cpp
@@ -7,56 +7,44 @@
 // Output callback register in Unity
 typedef void(__stdcall * OutputCallback) (uchar ** ptrs, int ptrSize, int * sizes, int sizeSize, int outputType);
 
-// Output type enum
-enum OutputType : int {
-	None = 0, 
-	Ids = 1, 
-	Name = 2, 
-	FrameNumber = 3, 
-	PoseKeypoints = 4, 
-	PoseIds = 5, 
-	PoseScores = 6, 
-	PoseHeatMaps = 7,
-	PoseCandidates = 8, 
-	FaceRectangles = 9, 
-	FaceKeypoints = 10, 
-	FaceHeatMaps = 11, 
-	HandRectangles = 12, 
-	HandKeypoints = 13, 
-	HandHeightMaps = 14, 
-	PoseKeypoints3D = 15, 
-	FaceKeypoints3D = 16, 
-	HandKeypoints3D = 17, 
-	CameraMatrix = 18, 
-	CameraExtrinsics = 19, 
-	CameraIntrinsics = 20
-};
-
-// Global setting structs
-std::shared_ptr<op::WrapperStructPose> spWrapperStructPose;
-std::shared_ptr<op::WrapperStructHand> spWrapperStructHand;
-std::shared_ptr<op::WrapperStructFace> spWrapperStructFace;
-std::shared_ptr<op::WrapperStructExtra> spWrapperStructExtra;
-std::shared_ptr<op::WrapperStructInput> spWrapperStructInput;
-std::shared_ptr<op::WrapperStructOutput> spWrapperStructOutput;
+// Global output callback
+OutputCallback unityOutputCallback;
+bool unityOutputEnabled = true;
 
 // This worker will just read and return all the jpg files in a directory
 class UnityPluginUserOutput : public op::WorkerConsumer<std::shared_ptr<std::vector<op::Datum>>> {
-public:
-	OutputCallback unityOutputCallback;
-	bool unityOutputEnabled = true;
-
+public:	
     void initializationOnThread() {}
 
-    void terminate() {
-		op::log("Stopping...");
-		unityOutputCallback = nullptr;
-        this->stop();
-    }
+	// Output type enum
+	enum OutputType : int {
+		None = 0,
+		Ids = 1,
+		Name = 2,
+		FrameNumber = 3,
+		PoseKeypoints = 4,
+		PoseIds = 5,
+		PoseScores = 6,
+		PoseHeatMaps = 7,
+		PoseCandidates = 8,
+		FaceRectangles = 9,
+		FaceKeypoints = 10,
+		FaceHeatMaps = 11,
+		HandRectangles = 12,
+		HandKeypoints = 13,
+		HandHeightMaps = 14,
+		PoseKeypoints3D = 15,
+		FaceKeypoints3D = 16,
+		HandKeypoints3D = 17,
+		CameraMatrix = 18,
+		CameraExtrinsics = 19,
+		CameraIntrinsics = 20
+	};
 
 protected:
 	template<class T>
 	void outputValue(T ** ptrs, int ptrSize, int * sizes, int sizeSize, OutputType outputType) {
+		if (!unityOutputCallback) return;
 		uchar ** bytePtrs = static_cast<uchar**>(static_cast<void*>(ptrs));
 		unityOutputCallback(bytePtrs, ptrSize, sizes, sizeSize, (int)outputType);
 	}
@@ -68,10 +56,10 @@ protected:
 					sendPoseKeypoints(datumsPtr);
 					sendHandKeypoints(datumsPtr);
 					sendFaceKeypoints(datumsPtr);
+					sendFaceRectangles(datumsPtr);
 				}
 			}
 		} catch (const std::exception& e) {
-			// this->stop();
 			op::error(e.what(), __LINE__, __FUNCTION__, __FILE__);
 		}
 	}
@@ -93,6 +81,7 @@ protected:
 			auto sizeVector = data[0].getSize();
 			int sizeSize = sizeVector.size();
 			int * sizes = &sizeVector[0];
+			//float ptrs[] = { data[0].getPtr(), data[1].getPtr() };
 			auto ptrs = new float*[2];
 			ptrs[0] = data[0].getPtr();
 			ptrs[1] = data[1].getPtr();	
@@ -111,16 +100,108 @@ protected:
 			outputValue(&val, 1, sizes, sizeSize, FaceKeypoints);
 		}
 	}
+
+	void sendFaceRectangles(const std::shared_ptr<std::vector<op::Datum>>& datumsPtr) {
+		auto& data = datumsPtr->at(0).faceRectangles; // std::vector<Rectangle<float>>
+		if (data.size() > 0) {
+			int sizes[] = { data.size(), 4 };
+			std::vector<float> vals(data.size() * 4);
+			for (int i = 0; i < data.size(); i++) {
+				vals[4 * i + 0] = data[i].x;
+				vals[4 * i + 1] = data[i].y;
+				vals[4 * i + 2] = data[i].width;
+				vals[4 * i + 3] = data[i].height;
+			}
+			float * val = &vals[0];
+			outputValue(&val, 1, sizes, 2, FaceRectangles);
+		}
+	}
 };
 
 // Global user output
-UnityPluginUserOutput* ptrUserOutput;
+UnityPluginUserOutput* ptrUserOutput = nullptr;
 
-// Title
-void openpose_main(bool enableOutput, OutputCallback callback);
+// Global setting structs
+std::shared_ptr<op::WrapperStructPose> spWrapperStructPose;
+std::shared_ptr<op::WrapperStructHand> spWrapperStructHand;
+std::shared_ptr<op::WrapperStructFace> spWrapperStructFace;
+std::shared_ptr<op::WrapperStructExtra> spWrapperStructExtra;
+std::shared_ptr<op::WrapperStructInput> spWrapperStructInput;
+std::shared_ptr<op::WrapperStructOutput> spWrapperStructOutput;
 
-// Unity Interface
+// Main
+void openpose_main() {
+	try {
+		// Starting
+		if (ptrUserOutput) return;
+		op::log("Starting OpenPose demo...", op::Priority::High);
+		const auto timerBegin = std::chrono::high_resolution_clock::now();
+
+		// OpenPose wrapper
+		auto spWrapper = std::make_shared<op::Wrapper>();
+
+		// Initializing the user custom classes
+		auto spUserOutput = std::make_shared<UnityPluginUserOutput>();
+		ptrUserOutput = spUserOutput.get();
+		
+		// Add custom processing
+		const auto workerOutputOnNewThread = true;
+		spWrapper->setWorker(op::WorkerType::Output, spUserOutput, workerOutputOnNewThread);
+
+		// Apply configurations
+		spWrapper->configure(*spWrapperStructPose);
+		spWrapper->configure(*spWrapperStructHand);
+		spWrapper->configure(*spWrapperStructFace);
+		spWrapper->configure(*spWrapperStructExtra);
+		spWrapper->configure(*spWrapperStructInput);
+		spWrapper->configure(*spWrapperStructOutput);
+
+		// Start processing
+		op::log("Starting thread(s)...", op::Priority::High);
+		spWrapper->exec();
+
+		// Running ...... Ending
+
+		// Measuring total time
+		const auto now = std::chrono::high_resolution_clock::now();
+		const auto totalTimeSec = (double)std::chrono::duration_cast<std::chrono::nanoseconds>(now - timerBegin).count()
+			* 1e-9;
+		const auto message = "OpenPose demo successfully finished. Total time: "
+			+ std::to_string(totalTimeSec) + " seconds.";
+		op::log(message, op::Priority::High);
+
+		// Reset pointer
+		ptrUserOutput = nullptr;
+	}
+	catch (const std::exception& e)
+	{
+		op::error(e.what(), __LINE__, __FUNCTION__, __FILE__);
+	}
+}
+
+// Functions called from Unity
 extern "C" {
+	// Start openpose safely
+	OP_API void OP_Run() {
+		if (ptrUserOutput == nullptr) 
+			openpose_main();
+	}
+	// Stop openpose safely
+	OP_API void OP_Shutdown() {
+		if (ptrUserOutput != nullptr) {
+			op::log("Stopping...");
+			ptrUserOutput->stop();
+		}
+	}
+	// Register Unity output callback function
+	OP_API void OP_RegisterOutputCallback(OutputCallback callback) {
+		unityOutputCallback = callback;
+	}
+	// Enable/disable output callback
+	OP_API void OP_SetOutputEnable(bool enable) {
+		unityOutputEnabled = enable;
+	}
+	// Configs
 	OP_API void OP_ConfigurePose(
 		bool body_disable = false,
 		char* model_folder = "models/", int number_people_max = -1, // moved
@@ -207,24 +288,24 @@ extern "C" {
 		}
 	}
 	OP_API void OP_ConfigureInput(
-		int frame_first = 0, int frame_last = -1, // unsigned long long (uint64)
+		uchar producerType = 5, char* producerString = "",
+		unsigned long long frame_first = 0, 
+		unsigned long long frame_step = 1, 
+		unsigned long long  frame_last = std::numeric_limits<unsigned long long>::max(),
 		bool process_real_time = false, bool frame_flip = false,
-		int frame_rotate = 0, bool frames_repeat = false,
-		// Producer
-		char* image_dir = "", char* video = "", char* ip_camera = "", int camera = -1,
-		bool flir_camera = false, int camera_resolution_x = -1, int camera_resolution_y = -1, double camera_fps = 30.0,
-		char* camera_parameter_folder = "models / cameraParameters / flir / ", bool frame_keep_distortion = false,
-		int _3d_views = 1, int flir_camera_index = -1) {
+		int frame_rotate = 0, bool frames_repeat = false, 
+		int camera_resolution_x = -1, int camera_resolution_y = -1, double webcam_fps = 30., 
+		char* camera_parameter_path = "models/cameraParameters/", 
+		bool undistort_image = true, uint image_directory_stereo = 1) {
 		try {
-			auto producerSharedPtr = op::flagsToProducer(
-				image_dir, video, ip_camera, camera,
-				flir_camera, std::to_string(camera_resolution_x) + "x" + std::to_string(camera_resolution_y), camera_fps,
-				camera_parameter_folder, !frame_keep_distortion,
-				(unsigned int)_3d_views, flir_camera_index);
-
 			spWrapperStructInput = std::make_shared<op::WrapperStructInput>(
-				producerSharedPtr, (unsigned long long) frame_first, (unsigned long long) frame_last, process_real_time, frame_flip,
-				frame_rotate, frames_repeat);
+				(op::ProducerType) producerType, producerString,
+				frame_first, frame_step, frame_last, 
+				process_real_time, frame_flip,
+				frame_rotate, frames_repeat,
+				op::Point<int>{ camera_resolution_x, camera_resolution_y }, webcam_fps,
+				camera_parameter_path,
+				undistort_image, image_directory_stereo);
 			//spWrapper->configure(spWrapperStructInput);
 		}
 		catch (const std::exception& e)
@@ -257,83 +338,6 @@ extern "C" {
 			op::error(e.what(), __LINE__, __FUNCTION__, __FILE__);
 		}
 	}
-
-	OP_API void OP_Run(bool enableOutput, OutputCallback callback) {
-		openpose_main(enableOutput, callback);
-	}
-
-	OP_API void OP_Shutdown() {
-		//if (opRunningFlag) opStoppingFlag = true;
-		if (ptrUserOutput) ptrUserOutput->terminate();
-	}
-
-	//OP_API void OP_RegisterOutputCallback(OutputCallback callback) {
-	//	//if (spUserOutput) spUserOutput->gOutputCallback = callback;
-	//}
-	//OP_API void OP_SetOutputEnable(bool enable) {
-	//	//if (spUserOutput) spUserOutput->gOutputEnabled = enable;
-	//}
-	//OP_API void OP_SetParameters(int argc, char *argv[]) {
-	//	//gflags::ParseCommandLineFlags(&argc, &argv, true);
-	//}
-	//OP_API void OP_LogTest() { op::log("test"); }
-	//OP_API void OP_OutputTest() { spUserOutput->outputTest(); }
 }
 
-// Main
-void openpose_main(bool enableOutput, OutputCallback callback) {
-	try {
-		// Starting
-		if (ptrUserOutput) return;
-		op::log("Starting OpenPose demo...", op::Priority::High);
-		const auto timerBegin = std::chrono::high_resolution_clock::now();
-
-		// OpenPose wrapper
-		auto spWrapper = std::make_shared<op::Wrapper>();
-
-		// Initializing the user custom classes
-		auto spUserOutput = std::make_shared<UnityPluginUserOutput>();
-		ptrUserOutput = spUserOutput.get();
-
-		// Register Unity output callback
-		spUserOutput->unityOutputEnabled = enableOutput;
-		if (callback) spUserOutput->unityOutputCallback = callback;
-
-		// Add custom processing
-		const auto workerOutputOnNewThread = true;
-		spWrapper->setWorker(op::WorkerType::Output, spUserOutput, workerOutputOnNewThread);
-
-		// TODO: fix Producer ptr problem to enable Unity users to set this in Unity (then delete this code)
-		OP_ConfigureInput();
-
-		// Apply configurations
-		spWrapper->configure(*spWrapperStructPose);
-		spWrapper->configure(*spWrapperStructHand);
-		spWrapper->configure(*spWrapperStructFace);
-		spWrapper->configure(*spWrapperStructExtra);
-		spWrapper->configure(*spWrapperStructInput);
-		spWrapper->configure(*spWrapperStructOutput);
-
-		// Start processing
-		op::log("Starting thread(s)...", op::Priority::High);
-		spWrapper->exec();
-
-		// Running ...... Ending
-
-		// Measuring total time
-		const auto now = std::chrono::high_resolution_clock::now();
-		const auto totalTimeSec = (double)std::chrono::duration_cast<std::chrono::nanoseconds>(now - timerBegin).count()
-			* 1e-9;
-		const auto message = "OpenPose demo successfully finished. Total time: "
-			+ std::to_string(totalTimeSec) + " seconds.";
-		op::log(message, op::Priority::High);
-
-		// Reset pointer
-		ptrUserOutput = nullptr;
-	}
-	catch (const std::exception& e)
-	{
-		op::error(e.what(), __LINE__, __FUNCTION__, __FILE__);
-	}
-}
 #endif
