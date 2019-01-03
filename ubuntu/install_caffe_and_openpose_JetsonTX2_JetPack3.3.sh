f478917491b0ffb1cc8d9c594c1426260571ab1e
Leo Liu <yla416@sfu.ca>
2019-01-03 09:35:45 -0800

    Add files for install under Jetpack 3.3 (#989)

---
+++ ubuntu/install_caffe_and_openpose_JetsonTX2_JetPack3.3.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+
+
+echo "------------------------- Installing Caffe and OpenPose -------------------------"
+echo "NOTE: This script assumes that just flashed JetPack 3.3 : Ubuntu 16, CUDA 9, cuDNN 7 and OpenCV are already installed on your machine. Otherwise, it might fail."
+
+
+
+function exitIfError {
+    if [[ $? -ne 0 ]] ; then
+        echo ""
+        echo "------------------------- -------------------------"
+        echo "Errors detected. Exiting script. The software might have not been successfully installed."
+        echo "------------------------- -------------------------"
+        exit 1
+    fi
+}
+
+
+
+function executeShInItsFolder {
+    # $1 = sh file name
+    # $2 = folder where the sh file is
+    # $3 = folder to go back
+    cd $2   
+    exitIfError
+    sudo chmod +x $1
+    exitIfError
+    bash ./$1
+    exitIfError
+    cd $3
+    exitIfError
+}
+
+
+rm -r ./3rdparty/caffe
+git submodule update --init --recursive
+mv ./ubuntu/install_caffe_JetsonTX2_JetPack3.3.sh ./3rdparty/caffe/install_caffe_JetsonTX2_JetPack3.3.sh
+mv ./ubuntu/Makefile.config.Ubuntu16_cuda9_JetsonTX2_caffe ./3rdparty/caffe/Makefile.config.Ubuntu16_cuda9_JetsonTX2_caffe
+executeShInItsFolder "install_caffe_JetsonTX2_JetPack3.3.sh" "./3rdparty/caffe" "../.."
+exitIfError
+
+
+
+executeShInItsFolder "install_openpose_JetsonTX2_JetPack3.3.sh" "./ubuntu" "./"
+exitIfError
+
+
+
+echo "------------------------- Caffe and OpenPose Installed -------------------------"
+echo ""
