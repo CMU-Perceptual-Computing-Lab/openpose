d5650df926dbfa28221b8b9f8e9c4790803f5e59
Gines Hidalgo <gineshidalgo99@gmail.com>
2019-01-04 10:49:27 -0500

    Fixes

---
+++ scripts/travis/install_deps_ubuntu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install dependencies for Ubuntu
+echo "Running on ${TRAVIS_OS_NAME} OS."
+
+BASEDIR=$(dirname $0)
+source $BASEDIR/defaults.sh
+
+if $WITH_CUDA ; then
+  sudo bash ./3rdparty/ubuntu/install_ubuntu_deps_and_cuda.sh
+else # if ! $WITH_CUDA ; then
+  sudo bash ./3rdparty/ubuntu/install_ubuntu_deps.sh
+fi
+sudo apt-get -y install libatlas-base-dev
+sudo apt-get -y install libopencv-dev
