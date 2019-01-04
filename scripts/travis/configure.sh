c7a97172d937948012994e106d0032e8adff632f
Gines Hidalgo <gineshidalgo99@gmail.com>
2019-01-04 09:30:32 -0500

    Multi-OS?

--- scripts/travis/configure.sh
+++ scripts/travis/configure.sh
@@ -4,8 +4,8 @@
 BASEDIR=$(dirname $0)
 source $BASEDIR/defaults.sh
 
-if ! $WITH_CMAKE ; then
-  source $BASEDIR/configure-make.sh
-else
+if $WITH_CMAKE ; then
   source $BASEDIR/configure-cmake.sh
+else # if ! $WITH_CMAKE ; then
+  source $BASEDIR/configure-make.sh
 fi
--- scripts/travis/run_make.sh
+++ scripts/travis/run_make.sh
@@ -7,8 +7,6 @@ source $BASEDIR/defaults.sh
 if $WITH_CMAKE ; then
   cd build
   make -j`nproc`
-  # make --jobs $NUM_THREADS
 else # if ! $WITH_CMAKE ; then
   make all -j`nproc`
-  # make --jobs $NUM_THREADS all
 fi
