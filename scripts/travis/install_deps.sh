1a556b8f9627b6f8a1c2b4cf30d63b322addb011
Gines Hidalgo <gineshidalgo99@gmail.com>
2019-01-04 10:36:23 -0500

    CUDA only installed for CUDA version

---
+++ scripts/travis/install_deps.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Install dependencies
+echo "Running on OS ${TRAVIS_OS_NAME}."
+
+# Ubuntu
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+  BASEDIR=$(dirname $0)
+  source $BASEDIR/defaults.sh
+
+  if $WITH_CUDA ; then
+    sudo bash ./3rdparty/ubuntu/install_ubuntu_deps_and_cuda.sh
+  else # if ! $WITH_CUDA ; then
+    sudo bash ./3rdparty/ubuntu/install_ubuntu_deps.sh
+  fi
+  sudo apt-get -y install libatlas-base-dev
+  sudo apt-get -y install libopencv-dev
+
+
+
+# OSX
+elif [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+  echo "Unknown OS ${TRAVIS_OS_NAME}."
+  # Install some custom requirements on OSX
+
+
+
+# Windows
+elif [[ $TRAVIS_OS_NAME == 'windows' ]]; then
+  echo "Unknown OS ${TRAVIS_OS_NAME}."
+  # Install some custom requirements on Windows
+
+
+
+# Unknown
+else
+  echo "Unknown OS ${TRAVIS_OS_NAME}."
+  exit 99
+fi
