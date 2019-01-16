beb124b9c65d6120d65989726ee6f393f90dabd6
Raaj <yaadhavraaj@gmail.com>
2019-01-16 17:17:03 -0500

    make travis mac work j1

--- scripts/travis/run_make.sh
+++ scripts/travis/run_make.sh
@@ -7,7 +7,8 @@ source $BASEDIR/defaults.sh
 
 if [[ $WITH_CMAKE == true ]] ; then
   cd build
-  make -j`nproc`
+  if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then make -j1 ; fi
+  if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then make -j`nproc` ; fi
 else
   make all -j`nproc`
 fi
