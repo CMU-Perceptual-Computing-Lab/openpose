ce6338f8a6694396990f77c19bbc3d003b12f9b9
Nico Zevallos <gnastacast@gmail.com>
2019-01-17 11:58:26 -0500

    Changed behaviour when encountering import errors for easier debugging. (#1031)

--- examples/tutorial_api_python/openpose_python.py
+++ examples/tutorial_api_python/openpose_python.py
@@ -21,8 +21,9 @@ try:
         # If you run `make install` (default path is `/usr/local/python` for Ubuntu), you can also access the OpenPose/python module from there. This will install OpenPose and the python library at your desired installation path. Ensure that this is in your python path in order to use it.
         # sys.path.append('/usr/local/python')
         from openpose import pyopenpose as op
-except:
-    raise Exception('Error: OpenPose library could not be found. Did you enable `BUILD_PYTHON` in CMake and have this Python script in the right folder?')
+except ImportError as e:
+    print('Error: OpenPose library could not be found. Did you enable `BUILD_PYTHON` in CMake and have this Python script in the right folder?')
+    raise e
 
 # Flags
 parser = argparse.ArgumentParser()
