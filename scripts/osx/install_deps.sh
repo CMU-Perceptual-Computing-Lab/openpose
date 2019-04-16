0342d4b36efe000b04d8f14b57bddb754d29f6fa
mram <mram@umd.edu>
2019-04-16 16:26:28 -0400

    Separated mac requirements from installation script

--- scripts/osx/install_deps.sh
+++ scripts/osx/install_deps.sh
@@ -1,12 +1,7 @@
-brew install openblas
-brew install -vd snappy leveldb gflags glog szip lmdb
-# need the homebrew science source for OpenCV and hdf5
-brew install hdf5 opencv
+cat scripts/osx/deps-core.txt | xargs brew install -vd
 # with Python pycaffe needs dependencies built from source
 #brew install --build-from-source --with-python -vd protobuf
 #brew install --build-from-source -vd boost boost-python
 # without Python the usual installation suffices
-brew install protobuf boost
-brew install cmake
-brew install viennacl
+cat scripts/osx/deps-rem.txt | xargs brew install -vd
 sudo pip install -r scripts/osx/python-requirements.txt
