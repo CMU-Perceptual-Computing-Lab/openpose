5f4d9dcf4c92ec1d73323402cada0904844ec416
mram <mram@umd.edu>
2019-04-16 15:06:52 -0400

    Specifying full path

--- scripts/ubuntu/install_deps.sh
+++ scripts/ubuntu/install_deps.sh
@@ -5,19 +5,19 @@
 sudo apt-get -y update
 
 # Core
-cat deps-core.txt | xargs sudo apt-get -y --no-install-recommends install
+cat scripts/ubuntu/deps-core.txt | xargs sudo apt-get -y --no-install-recommends install
 # General dependencies
-cat deps-gen.txt | xargs sudo apt-get -y --no-install-recommends install
+cat scripts/ubuntu/deps-gen.txt | xargs sudo apt-get -y --no-install-recommends install
 # Remaining dependencies, 14.04
-cat deps-rem.txt | xargs sudo apt-get -y --no-install-recommends install
+cat scripts/ubuntu/deps-rem.txt | xargs sudo apt-get -y --no-install-recommends install
 # Python2 libs
-cat deps-python2.txt | xargs sudo apt-get -y --no-install-recommends install
+cat scripts/ubuntu/deps-python2.txt | xargs sudo apt-get -y --no-install-recommends install
 sudo easy_install pip
-sudo -H pip install --upgrade -r python-requirements.txt
+sudo -H pip install --upgrade -r scripts/ubuntu/python-requirements.txt
 # Python3 libs
-cat deps-python3.txt | xargs sudo apt-get -y --no-install-recommends install
-sudo -H pip3 install --upgrade -r python-requirements.txt
+cat scripts/ubuntu/deps-python3.txt | xargs sudo apt-get -y --no-install-recommends install
+sudo -H pip3 install --upgrade -r scripts/ubuntu/python-requirements.txt
 # OpenCV 2.4 -> Added as option
 # # sudo apt-get -y install libopencv-dev
 # OpenCL Generic
-cat deps-opencl.txt | xargs sudo apt-get -y --no-install-recommends install
+cat scripts/ubuntu/deps-opencl.txt | xargs sudo apt-get -y --no-install-recommends install
