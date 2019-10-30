c422da55b87c248df1b1f0016c9de506640bc4b9
Lim Xiang Yann <xiangyann@hotmail.com>
2019-10-30 21:43:12 +0800

    non-efficient j1 to hw.logicalcpu

--- scripts/travis/run_make.sh
+++ scripts/travis/run_make.sh
@@ -8,8 +8,8 @@ source $BASEDIR/defaults.sh
 if [[ $WITH_CMAKE == true ]] ; then
   cd build
   if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then make -j`nproc` ; fi
-  if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then make -j1 ; fi
+  if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then make -j`sysctl -n hw.logicalcpu` ; fi
 else
   if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then make all -j`nproc` ; fi
-  if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then make all -j1 ; fi
+  if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then make all -j`sysctl -n hw.logicalcpu` ; fi
 fi
