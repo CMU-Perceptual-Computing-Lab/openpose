0bbe26d204518b579be69cff09a3fcce0cf6a9be
Ali Zahid Raja <alizahidrajaa@gmail.com>
2019-12-18 00:28:32 +0500

    Update install_cuda.sh for Ubuntu 18.04

--- scripts/ubuntu/install_cuda.sh
+++ scripts/ubuntu/install_cuda.sh
@@ -11,6 +11,14 @@ if [[ $ubuntu_version == *"14."* ]]; then
 elif [[ $ubuntu_version == *"16."* ]]; then
   wget -c "https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb"
   sudo dpkg --install cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
+elif [[ $ubuntu_version == *"18."* ]]; then
+  wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+  sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+  wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
+  sudo dpkg -i cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
+  sudo apt-key add /var/cuda-repo-10-2-local-10.2.89-440.33.01/7fa2af80.pub
+  sudo apt-get update
+  sudo apt-get -y install cuda
 fi
 sudo apt-get update
 sudo apt-get install cuda-8-0
