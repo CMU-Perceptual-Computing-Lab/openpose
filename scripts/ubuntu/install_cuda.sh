fe612a5b6777b6d52ec77057a566e83e814f877a
Cengizhan Pasaoglu <cengizhanpasaoglu@gmail.com>
2020-09-06 20:58:51 +0300

    Update script for downloading CUDA 11 for Ubuntu 20 or later versions

--- scripts/ubuntu/install_cuda.sh
+++ scripts/ubuntu/install_cuda.sh
@@ -25,5 +25,14 @@ elif [[ $ubuntu_version == *"18."* ]]; then
   sudo apt-key add /var/cuda-repo-10-2-local-10.2.89-440.33.01/7fa2af80.pub
   sudo apt-get update
   sudo apt-get -y install cuda
+# Install CUDA 11.0
+elif [[ $ubuntu_version == *"20."* ]]; then
+  wget -c "https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin"
+  sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+  wget "https://developer.download.nvidia.com/compute/cuda/11.0.3/local_installers/cuda-repo-ubuntu2004-11-0-local_11.0.3-450.51.06-1_amd64.deb"
+  sudo dpkg -i cuda-repo-ubuntu2004-11-0-local_11.0.3-450.51.06-1_amd64.deb
+  sudo apt-key add /var/cuda-repo-ubuntu2004-11-0-local/7fa2af80.pub
+  sudo apt-get update
+  sudo apt-get -y install cuda
 fi
 # sudo apt-get install cuda
