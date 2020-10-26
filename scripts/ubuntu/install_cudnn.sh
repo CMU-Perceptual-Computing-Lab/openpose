4bf5bb2ebb00dfe246a3ba2b8a8df284f7ddf345
Simon Uhrmann <suhrmann@posteo.de>
2020-10-26 19:31:30 +0100

    Show a warning and exit instead of downloading and installing cuDNN on Ubuntu > 16

--- scripts/ubuntu/install_cudnn.sh
+++ scripts/ubuntu/install_cudnn.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-echo "NOTE: This script assumes Ubuntu 16 or 14 and Nvidia Graphics card up to 10XX. Otherwise, install it by yourself or it will fail."
+ubuntu_version="$(lsb_release -r)"
+
+if [[ $ubuntu_version != *"14."* ]] && [[ $ubuntu_version != *"16."* ]]; then
+  # Ubuntu 14,16 display warning and exit
+  echo "NOTE: This script assumes Ubuntu 16 or 14 and Nvidia Graphics card up to 10XX. Otherwise, install it by yourself or it will fail."
+  echo "  See https://developer.nvidia.com/cudnn"
+  exit
+fi
 
 # Install cuDNN 5.1
 CUDNN_URL="http://developer.download.nvidia.com/compute/redist/cudnn/v5.1/cudnn-8.0-linux-x64-v5.1.tgz"
