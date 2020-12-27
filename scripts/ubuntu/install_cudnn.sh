4aca67b69e44e46eac0c8c6ce05d8954f6d964f4
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-12-27 15:59:43 +0100

    echo wget commands in scripts

--- scripts/ubuntu/install_cudnn.sh
+++ scripts/ubuntu/install_cudnn.sh
@@ -9,10 +9,11 @@ fi
 
 # Install cuDNN 5.1
 if [[ $UBUNTU_VERSION == *"14."* ]] || [[ $UBUNTU_VERSION == *"15."* ]] || [[ $UBUNTU_VERSION == *"16."* ]]; then
-	CUDNN_URL="http://developer.download.nvidia.com/compute/redist/cudnn/v5.1/cudnn-8.0-linux-x64-v5.1.tgz"
-	wget -c ${CUDNN_URL} ${WGET_VERBOSE}
-	sudo tar -xzf cudnn-8.0-linux-x64-v5.1.tgz -C /usr/local
-	rm cudnn-8.0-linux-x64-v5.1.tgz && sudo ldconfig
+    CUDNN_URL="http://developer.download.nvidia.com/compute/redist/cudnn/v5.1/cudnn-8.0-linux-x64-v5.1.tgz"
+    echo "wget -c ${CUDNN_URL} ${WGET_VERBOSE}"
+    wget -c ${CUDNN_URL} ${WGET_VERBOSE}
+    sudo tar -xzf cudnn-8.0-linux-x64-v5.1.tgz -C /usr/local
+    rm cudnn-8.0-linux-x64-v5.1.tgz && sudo ldconfig
 else
-	echo "cuDNN NOT INSTALLED! Ubuntu 16 or 14 not found. Install cuDNN manually from 'https://developer.nvidia.com/cudnn'."
+    echo "cuDNN NOT INSTALLED! Ubuntu 16 or 14 not found. Install cuDNN manually from 'https://developer.nvidia.com/cudnn'."
 fi
