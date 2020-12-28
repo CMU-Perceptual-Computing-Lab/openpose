48b444c77caab2417267cde3f95342142dc010e4
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-12-28 15:17:00 +0100

    Only install cudnn if enabled

--- scripts/travis/install_deps_ubuntu.sh
+++ scripts/travis/install_deps_ubuntu.sh
@@ -8,9 +8,14 @@ source "$BASEDIR"/defaults.sh
 
 if [[ "$WITH_CUDA" == "true" ]]
 then
-  source scripts/ubuntu/install_deps_and_cuda.sh
-else
-  source scripts/ubuntu/install_deps.sh
+  source scripts/ubuntu/install_cuda.sh
 fi
+if [[ "$WITH_CUDNN" == "true" ]]
+then
+  source scripts/ubuntu/install_cudnn.sh
+fi
+
+source scripts/ubuntu/install_deps.sh
+
 sudo apt-get -yq  install libatlas-base-dev
 sudo apt-get -yq  install libopencv-dev
