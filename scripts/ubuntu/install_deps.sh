82139a10613dff385af5e0420e2bb682680d4989
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-12-28 16:42:18 +0100

    Dont install python libs in CI if not used; Install opencv-python as user in CI only

--- scripts/ubuntu/install_deps.sh
+++ scripts/ubuntu/install_deps.sh
@@ -12,23 +12,34 @@ sudo apt-get -yq install libatlas-base-dev libprotobuf-dev libleveldb-dev libsna
 sudo apt-get -yq install --no-install-recommends libboost-all-dev
 # Remaining dependencies, 14.04
 sudo apt-get -yq install libgflags-dev libgoogle-glog-dev liblmdb-dev
-# Python2 libs (Official Ubuntu support dropped after Ubuntu 20)
-if [[ $UBUNTU_VERSION == *"14."* ]] || [[ $UBUNTU_VERSION == *"15."* ]] || [[ $UBUNTU_VERSION == *"16."* ]] || [[ $UBUNTU_VERSION == *"17."* ]] || [[ $UBUNTU_VERSION == *"18."* ]];
+if [[ ! $WITH_PYTHON == false ]]
 then
-    sudo apt-get -yq install python-setuptools python-dev build-essential
-    hash pip2 2> /dev/null || sudo apt-get -yq install python-pip
-    sudo -H pip2 install pip --upgrade
-    sudo -H pip2 install --upgrade "numpy<1.17" protobuf
-    pip2 install pip --upgrade --user
-    pip2 install --upgrade --user "opencv-python<4.3"
-else
-# Python3 libs
-    sudo apt-get -yq install python3-setuptools python3-dev build-essential
-    hash pip3 2> /dev/null || sudo apt-get -yq install python3-pip
-    sudo -H pip3 install pip --upgrade
-    sudo -H pip3 install --upgrade numpy protobuf
-    pip3 install pip --upgrade --user
-    pip3 install --upgrade --user opencv-python
+    # Python2 libs (Official Ubuntu support dropped after Ubuntu 20)
+    if [[ $UBUNTU_VERSION == *"14."* ]] || [[ $UBUNTU_VERSION == *"15."* ]] || [[ $UBUNTU_VERSION == *"16."* ]] || [[ $UBUNTU_VERSION == *"17."* ]] || [[ $UBUNTU_VERSION == *"18."* ]];
+    then
+        sudo apt-get -yq install python-setuptools python-dev build-essential
+        hash pip2 2> /dev/null || sudo apt-get -yq install python-pip
+        sudo -H pip2 install pip --upgrade
+        if [[ "$CI" == "true" ]]
+        then
+            sudo -H pip2 install --upgrade "numpy<1.17" protobuf
+            sudo -H pip2 install --user "opencv-python<4.3"
+        else
+            sudo -H pip2 install --upgrade "numpy<1.17" protobuf "opencv-python<4.3"
+        fi
+    else
+    # Python3 libs
+        sudo apt-get -yq install python3-setuptools python3-dev build-essential
+        hash pip3 2> /dev/null || sudo apt-get -yq install python3-pip
+        sudo -H pip3 install pip --upgrade
+        if [[ "$CI" == "true" ]]
+        then
+            sudo -H pip3 install --upgrade numpy protobuf
+            sudo -H pip3 install --user opencv-python
+        else
+            sudo -H pip3 install --upgrade numpy protobuf opencv-python
+        fi
+    fi
 fi
 # OpenCL Generic (Official OpenPose support dropped after Ubuntu 20)
 if [[ $UBUNTU_VERSION == *"14."* ]] || [[ $UBUNTU_VERSION == *"15."* ]] || [[ $UBUNTU_VERSION == *"16."* ]] || [[ $UBUNTU_VERSION == *"17."* ]] || [[ $UBUNTU_VERSION == *"18."* ]]
