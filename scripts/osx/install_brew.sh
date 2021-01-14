b13438502ca5fb423903b6dafe40dd431a34701a
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2021-01-15 00:28:57 +0100

    (OSX) replace outdated ruby installer

--- scripts/osx/install_brew.sh
+++ scripts/osx/install_brew.sh
@@ -1,3 +1,3 @@
 xcode-select --install
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew update
