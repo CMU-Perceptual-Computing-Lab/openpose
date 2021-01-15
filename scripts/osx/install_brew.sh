42425e4b115616b8fe0b43926a7d9d15f5b6496b
Ginés Hidalgo <gineshidalgo99@gmail.com>
2021-01-15 11:00:21 -0500

    Update install_brew.sh

--- scripts/osx/install_brew.sh
+++ scripts/osx/install_brew.sh
@@ -1,3 +1,4 @@
+# In case the command line have already been installed, "true" will make it return a non-zero error code to avoid stopping the scripts
 xcode-select --install || true
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew update
