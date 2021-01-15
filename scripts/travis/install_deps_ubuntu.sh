c3ea8250b16cb4039de01aeae20422b8de9d0f5e
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2021-01-15 20:41:56 +0100

    (CI)(U) remove unnecessary sudo

--- scripts/travis/install_deps_ubuntu.sh
+++ scripts/travis/install_deps_ubuntu.sh
@@ -7,13 +7,13 @@ BASEDIR=$(dirname $0)
 source $BASEDIR/defaults.sh
 
 if [[ $WITH_CUDA == true ]]; then
-  sudo bash $BASEDIR/../ubuntu/install_cuda.sh
+  bash $BASEDIR/../ubuntu/install_cuda.sh
 fi
 if [[ $WITH_CUDNN == true ]]; then
-  sudo bash $BASEDIR/../ubuntu/install_cudnn.sh
+  bash $BASEDIR/../ubuntu/install_cudnn.sh
 fi
 
-sudo bash $BASEDIR/../ubuntu/install_deps.sh
+bash $BASEDIR/../ubuntu/install_deps.sh
 
 sudo apt-get -y install libatlas-base-dev
 sudo apt-get -y install libopencv-dev
